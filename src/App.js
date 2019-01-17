89055efb19b3e024d6bfcbd54c4326f5194c9ec5
ZCLOVEYJ <394722003@qq.com>
2019-01-17 15:08:44 +0800

    changePage

--- src/App.js
+++ src/App.js
@@ -2,27 +2,33 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//获取图片相关的数据
+var imagesDatas = require('../src/data/imagesData.json');
+
+//利用自执行的函数，将图片名信息转成图片URL路径信息
+imagesDatas = (function genImageURL(imagesDatasArr) {
+  for (let i = 0, j = imagesDatasArr.length; i < j; i++) {
+    var simpleImageData = imagesDatasArr[i];
+    simpleImageData.imageURL = require('../src/images/' + simpleImageData.fileName);
+    imagesDatasArr[i] = simpleImageData;
+  }
+  return imagesDatasArr;
+})(imagesDatas);
+
+
+
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+
+      <section className="stage">
+        <section className="img-sec"> </section>
+        <nav className="controller-nav"></nav>
+      </section>
     );
   }
 }
 
 export default App;
+
